ca667adc440dd268a83c77e0e7dc8cc76786da99
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2018-02-05 19:18:44 -0200

    changed to shade sdk

--- deploy.py
+++ deploy.py
@@ -1,35 +1,20 @@
-from base64 import b64encode
-from openstack import connection
 import pystache
 import os
+import shade
+import json
+
+docker_registry = '10.51.0.39:5000'
+zipkin_host = '10.51.0.39'
+zipkin_port = 9411
+camera_ips = ['10.51.100.2', '10.51.100.3', '10.51.101.2', '10.51.101.3']
+base_image = 'docker-base'
+key_value_name = 'ninja'
 
 
-def load_environ_credentials():
-    return {
-        'auth_url': os.environ['OS_AUTH_URL'],
-        'project_name': os.environ['OS_PROJECT_NAME'],
-        'username': os.environ['OS_USERNAME'],
-        'password': os.environ['OS_PASSWORD'],
-        'project_id': os.environ['OS_PROJECT_ID'],
-        'project_domain_name': 'default',
-        'user_domain_name': os.environ['OS_USER_DOMAIN_NAME'],
-        'version': '2'
-    }
-
-
-def deploy(conn, image, name, args=[], env=[], key_pair='ninja', flavor='m1.tiny',
-           network='Net22', vm='docker-base-ntp', ip='', workdir='', before='', volume=''):
-    template = "#!/bin/bash\n"
-    template += "{{before}}\n"
-    template += "sudo docker pull {{image}}\n"
-    template += "sudo docker run -d --restart=always --network=host "
-    template += "{{#name}}--name {{.}}{{/name}} {{#workdir}}-w {{.}}{{/workdir}} "
-    template += "{{#volume}}-v {{.}}{{/volume}} {{#env}}-e {{.}} {{/env}} "
-    template += "{{image}} {{#args}}{{.}} {{/args}}"
-
-    init_script = pystache.render(template, {
-        'name': name,
-        'image': image,
+def docker_run(image, args=[], workdir='', volume='', env=[], before=''):
+    template = open('./docker_run.sh.mustache', 'r').read()
+    return pystache.render(template, {
+        'image': '{}/{}'.format(docker_registry, image),
         'args': args,
         'workdir': workdir,
         'before': before,
@@ -37,107 +22,92 @@ def deploy(conn, image, name, args=[], env=[], key_pair='ninja', flavor='m1.tiny
         'env': env,
     })
 
-    print init_script
 
-    try:
-        network = {'uuid': conn.network.find_network(network).id}
-        if ip != '':
-            network['fixed_ip'] = ip
-
-        return conn.compute.create_server(
+def create_server(cloud, name, userdata, wait=False):
+    server = cloud.get_server(name)
+    if server is None:
+        print '[INFO] Creating server {}'.format(name)
+        server = cloud.create_server(
             name=name,
-            flavor_id=conn.compute.find_flavor(flavor).id,
-            image_id=conn.compute.find_image(vm).id,
-            networks=[network],
-            key_name=key_pair,
-            user_data=b64encode(init_script)
+            image=base_image,
+            flavor=cloud.get_flavor('m1.small'),
+            key_name=key_value_name,
+            userdata=userdata,
         )
-    except:
-        ignore = raw_input("Failed to create server... Continue anyway? (y/n)")
-        if ignore == 'y':
-            return None
-        exit(-1)
+    else:
+        print '[INFO] Server {} already running'.format(name)
+
+    if wait == True:
+        cloud.wait_for_server(server)
+        server = cloud.get_server(name)
+    return server
 
 
-def create_floating_ip(conn):
-    provider_id = conn.network.find_network('provider').id
-    return conn.network.create_ip(floating_network_id=provider_id)
+shade.simple_logging(debug=False)
+cloud = shade.openstack_cloud(cloud='prod')
 
-
-def attach_floating_ip(conn, vm, ip):
-    conn.compute.wait_for_server(vm)
-    conn.compute.add_floating_ip_to_server(vm, address=ip.name)
-
-
-conn = connection.Connection(**load_environ_credentials())
-
-docker_registry = '10.61.100.253:5000'
-
-zipkin_ip = '10.61.100.253'
-zipkin_port = 9411
-
-broker_ip = '10.0.2.200'
-broker_uri = 'amqp://{}'.format(broker_ip)
-
-#### RabbitMQ
-vm = deploy(
-    conn,
-    image='{}/rabbitmq:3'.format(docker_registry),
+# RabbitMQ
+server = create_server(
+    cloud,
     name='RabbitMQ',
-    ip=broker_ip,
+    userdata=docker_run(image='is-rabbitmq:3'),
+    wait = True,
 )
+broker_uri = 'amqp://{}'.format(server.public_v4)
 
-ip = create_floating_ip(conn)
-attach_floating_ip(conn, vm, ip)
-broker_float_ip = ip.floating_ip_address
-broker_float_uri = 'amqp://{}'.format(broker_float_ip)
-print 'Broker ip: http://{}:15672'.format(broker_float_ip)
-
-#### Camera Gateways
-camera_ips = ['10.61.100.2', '10.61.100.3', '10.61.101.2', '10.61.101.3']
-for n in xrange(4):
-    vm = deploy(
-        conn,
-        image='{}/camera-gateway:1.1'.format(docker_registry),
+# Camera Gateways
+for n, camera in enumerate(camera_ips):
+    create_server(
+        cloud,
         name='CameraGateway.{}'.format(n),
-        args=["./service", "-i", n, "-c", camera_ips[n],
-              "-u", broker_float_uri, "-s", "1400", "-d", "6000",
-              "-z", zipkin_ip, "-p", zipkin_port],
-        network='provider',
+        userdata=docker_run(
+            image='camera-gateway:1.1',
+            args=["./service", "-i", n, "-c", camera, "-u", broker_uri,
+                  "-s", "1400", "-d", "6000", "-z", zipkin_host, "-p", zipkin_port],
+        ),
     )
 
-#### Mjpeg Server
-vm = deploy(
-    conn,
-    image='{}/mjpeg-server:1'.format(docker_registry),
+# Http Mjpeg Server
+create_server(
+    cloud,
     name='MjpegServer',
-    env=['IS_URI={}'.format(broker_uri)],
+    userdata=docker_run(
+        image='mjpeg-server:1',
+        env=['IS_URI={}'.format(broker_uri)],
+    )
 )
 
-#### Aruco Detector
+# Aruco Detector
 for n in xrange(4):
-    vm = deploy(
-        conn,
-        before="git clone https://github.com/labviros/is-aruco-calib",
-        volume="`pwd`/is-aruco-calib:/opt",
-        image='{}/aruco:1'.format(docker_registry),
+    create_server(
+        cloud,
         name='ArUco.{}'.format(n),
-        args=["./service", "-u", broker_uri, "-l", 0.3, "-d", 0, "-c", "/opt",
-              "-z", zipkin_ip, "-p", zipkin_port],
+        userdata=docker_run(
+            before="git clone https://github.com/labviros/is-aruco-calib",
+            volume="`pwd`/is-aruco-calib:/opt",
+            image='aruco:1',
+            args=["./service", "-u", broker_uri, "-l", 0.3, "-d", 0, "-c", "/opt",
+                  "-z", zipkin_host, "-p", zipkin_port],
+        )
     )
 
-#### Sync Service
-vm = deploy(
-    conn,
-    image='10.61.100.253:5000/time-sync:1',
+# Sync Service
+create_server(
+    cloud,
     name='Time.Sync',
-    args=["./service", "-u", broker_uri],
+    userdata=docker_run(
+        image='sync:1',
+        args=["./service", "-u", broker_uri],
+    )
 )
 
-#### Robot Controller
-vm = deploy(
-    conn,
-    image='10.61.100.253:5000/robot-controller:1',
+# Robot Controller
+create_server(
+    cloud,
     name='RobotController',
-    args=["./service", "-u", broker_uri, "-z", zipkin_ip, "-P", zipkin_port],
+    userdata=docker_run(
+        image='robot-controller:1',
+        args=["./service", "-u", broker_uri,
+              "-z", zipkin_host, "-P", zipkin_port],
+    )
 )
